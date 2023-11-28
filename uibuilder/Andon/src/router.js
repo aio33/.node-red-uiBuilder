225ce5d1a65daf751987459d05b534602a4070db
aio33 <jonathan.banon@aio.eu>
2023-11-28 14:38:32 +0100

    refacto : uibuilder delete old test

--- uibuilder/Andon/src/router.js
+++ uibuilder/Andon/src/router.js
@@ -1,35 +1,12 @@
-const Home = httpVueLoader('./components/subcomponents/home.vue');
-const Form = httpVueLoader('./components/subcomponents/form.vue');
-const Table = httpVueLoader('./components/subcomponents/table.vue');
-const Chart = httpVueLoader('./components/subcomponents/chart.vue');
+const Home = httpVueLoader('./components/home/home.vue');
 
 export default {
 
     routes: [
         {
-            path: '/',
-            name: 'Home',
-            components: Home
-        },
-        {
             path:'/home',
             name:'Home',
             component: Home
-        },
-        {
-            path:'/form',
-            name:'Form',
-            component: Form
-        },
-        {
-            path:'/table',
-            name:'Table',
-            component: Table
-        },
-        {
-            path:'/chart',
-            name:'Chart',
-            component: Chart
         }
     ],
 };
